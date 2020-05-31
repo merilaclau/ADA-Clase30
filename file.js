07355c0896e56c610e05fd8d627b946993296e85
Meri Laclau <merilaclau@gmail.com>
2020-05-31 03:48:44 -0300

    ajustes menores

--- file.js
+++ file.js
@@ -50,28 +50,3 @@ else if (largoPelo === "corto" && barba === "No") {
 else {
     alert("Si sólo tenés barba, no me hagas perder el tiempo. Rajá de acá hipster palermitano.");
 }
-
-
-
-
-// 03
-/******************************************************************************
- * 
- * 1. Pedir un mes del año por prompt en números 
- *    (ej: 12)
- * 2. Devolver el mes del año en texto (ej: "Diciembre")
- * 3. Hacer 2 veces, una con SWITCH Y otra 
- *    con IF-ELSE IF-ELSE
- * 
- ****************************************************************************/
-
- // 04
- /******************************************************************************
- * 
- * 1. Pedir un día de la semana por prompt en texto 
- * (ej: "Lunes")
- * 2. Devolver el número del día (ej: 2)
- * 3. En caso de que el día ingresado no exista, tirar
- *    un error y agarrarlo, e imprimirlo.
- * 
- ****************************************************************************/
